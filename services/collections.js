9b421a66b77ba1e39cd8bb9cf5eeefe1fb7447f9
Enrique Flores <ehf.81@hotmail.com>
2021-06-30 10:13:36 -0700

    update

--- services/collections.js
+++ services/collections.js
@@ -4,6 +4,7 @@ export const onSnapshot = (ref, callback, options) => {
     let items = snapshot.docs.map((doc) => {
       const data = doc.data();
       data.id = doc.id;
+      return data;
     });
     items = options && options.sort ? items.sort(options.sort) : items;
     callback(items);
